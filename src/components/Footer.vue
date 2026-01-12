dd2a51f124e0d2bedaa60f84ab61af41134a1bd3
dhave-dev13 <dhave.dev13@gmail.com>
2026-01-13 06:51:59 +0800

    first component usage

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import {ref} from 'vue'
+    const copyright_year = ref("2026")
+</script>
+
+<template>
+  <footer>
+    <p>&copy; {{copyright_year}}</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  padding-bottom: 20px;
+}
+footer p {
+  color: blue
+}
+</style>
