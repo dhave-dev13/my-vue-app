86a139c82fe7f0107836ed95675de5c073b2ff10
dhave-dev13 <dhave.dev13@gmail.com>
2026-01-13 06:55:33 +0800

    @ sign usage

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import Header from '@/components/Header.vue'
 import Main from '@/components/Main.vue'
 import Footer from '@/components/Footer.vue'
+
+//@ -- by Vite found in vite.config.js an alias for fileUrlToPath './src'
 </script>
 
 <template>
